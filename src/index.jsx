f50d39ef9439bfde3eb550f394e53044733e53a0
Tom Hirschfeld <tom@wi.co>
2015-09-29 10:45:04 -0700

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,131 @@
+import React from 'react/addons';
+import loadImage from './loadImage';
+
+export default class ExifImage extends React.Component {
+  constructor(props) {
+    super(props, ExifImage);
+
+    this.willRotate = [
+      'right-top',
+      'left-top',
+      'left-bottom',
+      'right-bottom'
+    ];
+
+    this.rotationStyleMap = {
+      'right-top': {
+        transform: 'rotate(0.25turn)',
+      },
+
+      'left-top': {
+        transform: 'rotate(0.25turn) scaleY(-1)',
+      },
+
+      'top-right': {
+        transform: 'scaleX(-1)',
+      },
+
+      'top-left': {},
+
+      'bottom-right': {
+        transform: 'rotate(0.5turn)',
+      },
+
+      'bottom-left': {
+        transform: 'rotate(0.5turn) scaleX(-1)',
+      },
+
+      'left-bottom': {
+        transform: 'rotate(-0.25turn)',
+      },
+
+      'right-bottom': {
+        transform: 'rotate(-0.25turn) scaleX(-1)',
+      }
+    };
+
+    this.state = {
+      imgStyle: {},
+      containerStyle: {}
+    };
+  }
+
+  static propTypes = {
+    urlValue: React.PropTypes.string,
+  }
+
+  fetchImage(imageUrl){
+    let xhr = new XMLHttpRequest();
+    xhr.open( 'GET', imageUrl, true );
+    xhr.responseType = 'arraybuffer';
+    const imagePromise = new Promise((resolve, reject) => {
+      xhr.onload = function(e) {
+        resolve({
+          response: this.response,
+          contentType: this.getResponseHeader('content-type')
+        });
+      };
+
+      xhr.onerror = function(e) {
+        reject(e);
+      };
+    });
+    xhr.send();
+    return imagePromise;
+  }
+
+  processImageBuffer({response, contentType}){
+    loadImage.parseMetaData(new Blob([response], {type: contentType}),
+      (data) => {
+        const orientation = data.exif.getAll().Orientation;
+        this.setState({
+          imgStyle: this.rotationStyleMap[orientation]
+        });
+        if(this.willRotate.indexOf(orientation) > -1){
+          const imgTag = React.findDOMNode(this.refs.img);
+          const containerStyle = Object
+            .assign(this.state.containerStyle, {
+              height: imgTag.offsetWidth
+          });
+          this.setState({
+            containerStyle
+          });
+        }
+      }
+    );
+  }
+
+  handleUrlUpdate(url){
+    return this
+      .fetchImage(url)
+      .then(this.processImageBuffer)
+      .catch((e) => {
+        console.error(e);
+      });
+  }
+
+  componentDidMount(){
+    this.handleUrlUpdate(this.props.urlValue);
+  }
+
+  componentDidUpdate(prevProps){
+    if(prevProps.urlValue !== this.props.urlValue){
+      this.handleUrlUpdate(this.props.urlValue);
+    }
+  }
+
+  render() {
+    return (
+      <div
+        className="wi-ExifImage-Container"
+        style={this.state.containerStyle}>
+        <img
+          {...this.props}
+          style={this.state.imgStyle}
+          ref="img"
+          src={this.props.urlValue}/>
+      </div>
+    );
+  }
+
+}
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const merge             = require('lodash.merge');
+const path              = require('path');
+const webpack           = require('webpack');
+const webpackConfig     = require('webpack-config');
+
+// DIRs & ENVs
+const SRC_DIR = path.resolve('./src');
+const BUILD_DIR = path.resolve('./build/');
+const NODE_MODULES_DIR = path.resolve('./node_modules');
+
+// Loaders
+const phonyExtractLoader = {
+  extract: function(value) {
+    return value;
+  },
+};
+
+const config = {
+  output: {
+    path: BUILD_DIR,
+    publicPath: '/',
+    sourcePrefix: '\t',
+    crossOriginLoading: 'anonymous',
+    library: 'testConfig',
+    libraryTarget: 'commonjs2'
+  },
+
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(true),
+  ],
+
+  resolve: {
+    alias: {
+      'react': path.join(__dirname, 'node_modules', 'react')
+    },
+    extensions: ['', '.webpack.js', '.web.js', '.js', '.jsx'],
+    root: SRC_DIR,
+  },
+
+  eslint: {
+    failOnError   : true,
+    failOnWarning : false,
+    emitError     : true,
+    emitWarning   : true,
+    formatter     : require('eslint-friendly-formatter'),
+  },
+
+  module: {
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        include: SRC_DIR,
+        loader: 'eslint-loader',
+      }
+    ],
+
+    loaders: [
+      {
+        test: /\.(jsx|js)?$/,
+        include: SRC_DIR,
+        loaders: ['babel-loader?optional[]=runtime']
+      }
+    ]
+  }
+};
+
+const appConfig = merge({}, config, {
+  entry: './src/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'index.js'
+  }
+
+});
+
+module.exports = webpackConfig.fromObject(appConfig);
